4c43fc86682053f037a0caa6d9c9e7db59084649
carla <carla.output@gmail.com>
2018-07-10 19:34:47 -0400

    Work in progress.

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',    # added by CarlaP.
-    'accounts'
+    'accounts',
+    'worldcup',
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -20,11 +20,11 @@ from . import views
 
 # Blog connexions by CarlaPastor
 urlpatterns = [
-    url(r"^$", views.HomePage.as_view(), name="home"),
-    url(r"^admin/", admin.site.urls),
-    url(r"^accounts/", include("accounts.urls", namespace="accounts")),
-    url(r"^accounts/", include("django.contrib.auth.urls")),
-    url(r"^test/$", views.TestPage.as_view(), name="test"),
-    url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
+    # url(r"^$", views.HomePage.as_view(), name="home"),
+    # url(r"^admin/", admin.site.urls),
+    # url(r"^accounts/", include("accounts.urls", namespace="accounts")),
+    # url(r"^accounts/", include("django.contrib.auth.urls")),
+    # url(r"^test/$", views.TestPage.as_view(), name="test"),
+    # url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
 ]
 
